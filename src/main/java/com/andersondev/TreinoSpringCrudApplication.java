4c9bc2e3078a620e04b37152e40f0e3e5c250736
andersonrocha1 <anderson.tec.informacao@gmail.com>
2023-02-19 19:51:04 -0300

    Arquivos iniciais

---
+++ src/main/java/com/andersondev/TreinoSpringCrudApplication.java
@@ -0,0 +1,13 @@
+package com.andersondev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinoSpringCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinoSpringCrudApplication.class, args);
+	}
+
+}
