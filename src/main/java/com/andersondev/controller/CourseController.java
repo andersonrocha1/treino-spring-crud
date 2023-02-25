fb55843b12240344fcdfefcae9f999e6feda7317
andersonrocha1 <anderson.tec.informacao@gmail.com>
2023-02-25 14:07:32 -0300

    Fazendo a validação com Bean Validation

--- src/main/java/com/andersondev/controller/CourseController.java
+++ src/main/java/com/andersondev/controller/CourseController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,9 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.andersondev.model.CourseModel;
 import com.andersondev.repository.CourseRepository;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 
-
+@Validated
 @RestController
 @RequestMapping("/api/courses")
 @AllArgsConstructor
@@ -37,7 +41,7 @@ public class CourseController {
 	
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
-	public CourseModel create(@RequestBody CourseModel course) {
+	public CourseModel create(@RequestBody @Valid CourseModel course) {
 		//Optei por usar a anotação em vez do ResponseEntity	
 		return courseRepository.save(course);
 		//return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
@@ -45,7 +49,7 @@ public class CourseController {
 	}
 	
 	@GetMapping("/{id}")
-	public ResponseEntity<CourseModel> findById(@PathVariable Long id) {
+	public ResponseEntity<CourseModel> findById(@PathVariable @NotNull @Positive Long id) {
 		
 		return courseRepository.findById(id)
 				.map(recordNotFound -> ResponseEntity.ok().body(recordNotFound))
@@ -54,7 +58,7 @@ public class CourseController {
 	
 	
 	@PutMapping("/{id}")
-	public ResponseEntity<CourseModel> update(@PathVariable Long id, @RequestBody CourseModel course){
+	public ResponseEntity<CourseModel> update(@PathVariable @NotNull @Positive Long id, @RequestBody @Valid CourseModel course){
 		
 		return courseRepository.findById(id)
 				.map(recordNotFound -> {
@@ -70,7 +74,7 @@ public class CourseController {
 	}
 	
 	@DeleteMapping("/{id}")
-	public ResponseEntity<Void> delete(@PathVariable Long id){
+	public ResponseEntity<Void> delete(@PathVariable @NotNull @Positive Long id){
 		
 		return courseRepository.findById(id)
 				.map(recordNotFound -> {
--- src/main/java/com/andersondev/model/CourseModel.java
+++ src/main/java/com/andersondev/model/CourseModel.java
@@ -2,6 +2,8 @@ package com.andersondev.model;
 
 import java.io.Serializable;
 
+import org.hibernate.validator.constraints.Length;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
@@ -10,6 +12,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -25,10 +30,15 @@ public class CourseModel implements Serializable{
 	@JsonProperty("_id")
 	private Long id;
 	
-	@Column(length = 200, nullable = false)
+	@NotBlank
+	@NotNull
+	@Length(min = 5, max = 120)
+	@Column(length = 120, nullable = false)
 	private String name;
 	
-	
+	@NotNull
+	@Length(max = 12)
+	@Pattern(regexp = "Front-End|Back-End")
 	@Column(length = 12, nullable = false)
 	private String category;
 	
