cb27c8b702f327c248354f97eff472d45f4cdc5f
andersonrocha1 <anderson.tec.informacao@gmail.com>
2023-02-25 15:08:00 -0300

    Deleção ou Remoção Lógica(Soft Delete)

--- src/main/java/com/andersondev/model/CourseModel.java
+++ src/main/java/com/andersondev/model/CourseModel.java
@@ -2,6 +2,8 @@ package com.andersondev.model;
 
 import java.io.Serializable;
 
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -19,6 +21,8 @@ import lombok.Data;
 
 @Data
 @Entity
+@SQLDelete(sql = "UPDATE tb_courses SET status = 'Inativo' WHERE id = ? ")
+@Where(clause = "status = 'Ativo'")
 @Table(name="tb_courses")
 public class CourseModel implements Serializable{
 
@@ -41,6 +45,12 @@ public class CourseModel implements Serializable{
 	@Pattern(regexp = "Front-End|Back-End")
 	@Column(length = 12, nullable = false)
 	private String category;
+	
+	@NotNull
+	@Length(max = 12)
+	@Pattern(regexp = "Ativo|Inativo")
+	@Column(length = 12, nullable = false)
+	private String status = "Ativo";
 	
 	
 }
